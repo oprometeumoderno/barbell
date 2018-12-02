c4ff154a33e72aa04e4f03df4634c1f3c5e9ebf9
Henrique Lopes <oprometeumoderno@gmail.com>
2018-12-01 23:27:37 -0200

    include static files in barbell package

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='barbell',
-    version='0.1',
+    version='0.2',
     scripts=['barbell'],
     author="Henrique de Paula",
     author_email="oprometeumoderno@gmail.com",
     description="A tool for creating Gym environments",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     url="https://github.com/oprometeumoderno/barbell",
     packages=setuptools.find_packages(),
     install_requires=['gym', 'stringcase'],
